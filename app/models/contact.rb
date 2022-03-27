89b3444d1c6b85ebc1e77345d3329823ea558159
Karine Vieira <kv.vieira1@gmail.com>
2022-03-27 15:53:11 -0300

    🌐 add i18n

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,11 @@ class Contact < ApplicationRecord
 
   validates :name, :email, presence: true
 
+  def locale
+    I18n.default_locale
+  end
+
   def as_json(options={})
-    super(root: true, include: { kind: { only: :description } })
+    super(root: true, include: { kind: { only: :description } }, methods: :locale)
   end
 end
