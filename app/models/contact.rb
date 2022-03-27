94653fffe50f7f13f8b2cf85f8d0aab3c72a2c0a
Karine Vieira <kv.vieira1@gmail.com>
2022-03-27 15:25:34 -0300

    ✨ allow create contact with kind

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,8 @@
 class Contact < ApplicationRecord
   belongs_to :kind
 
+  validates :name, :email, presence: true
+
   def as_json(options={})
     super(root: true, include: { kind: { only: :description } })
   end
