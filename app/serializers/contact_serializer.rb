1cb70f0208de689c7051547841d0683eb3485203
Karine Vieira <kv.vieira1@gmail.com>
2022-03-30 17:59:22 -0300

    ♻️ refactor code

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,13 +2,14 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
   belongs_to :kind do
-    link(:kind) { kind_path(object.kind.id) }
+    link(:related) { kind_path(object.kind.id) }
   end
-  
+
   has_many :phones
   has_one :address
 
   link(:self) { contact_path(object.id) }
+  link(:kind) { kind_path(object.kind.id) }
 
   def attributes(*args)
     current_hash = super(*args)
