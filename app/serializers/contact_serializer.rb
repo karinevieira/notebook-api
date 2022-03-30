ac5fd696e260c88bc90d09d2d194b22e2b1115e5
Karine Vieira <kv.vieira1@gmail.com>
2022-03-30 16:56:29 -0300

    ✨ add links in json

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,14 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
-  belongs_to :kind
+  belongs_to :kind do
+    link(:kind) { kind_path(object.kind.id) }
+  end
+  
   has_many :phones
   has_one :address
+
+  link(:self) { contact_path(object.id) }
 
   def attributes(*args)
     current_hash = super(*args)
